7f1a7c38dfa941bb4264c8f3f14b785569664d1e
lee-yujinn <leeyj7988@naver.com>
2020-10-17 17:13:12 +0900

    [Add] 2차세미나

---
+++ Seminar/SOPT_iOS_Seminar2/SOPT_iOS_Seminar2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SOPT_iOS_Seminar2
+//
+//  Created by 이유진 on 2020/10/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
